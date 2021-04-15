44fe4a9309de9fe8214dbaa3def7d21d33c27d2b
pawelswiat <pawelswiat89@gmail.com>
2021-04-15 17:33:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-m5qalo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-m5qalo)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import "./style.css";
+
+$("#btn-get-posts").click(() => getPosts());
+
+function getPosts() {
+  $.ajax({
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/posts/",
+    dataType: "json",
+    success: posts => appendPostToBody(posts)
+  });
+}
+
+function appendPostToBody(posts) {
+  posts
+    .filter((post, index) => index < 15)
+    .forEach(post => {
+      $("body").append(getPostsAndAppendToBody(post));
+    });
+}
+
+function getPostsAndAppendToBody(postData) {
+  return `
+    	<div>
+      	<p>Post id: ${postData.id}</p>
+        <p>User id: ${postData.userId}</p>
+        <label for="title">Post title: </label>
+        	<input type="text" name="title" value="${postData.title}">
+				<label for="body">Post body: </label>
+        	<textarea name="body">${postData.body}</textarea>
+      </div>
+    `;
+}
